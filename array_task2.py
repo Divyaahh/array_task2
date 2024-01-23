7144e599682f6449cc2a2eb8e175943784a1f625
dimar <dimar@DIVYA>
2024-01-23 20:54:01 +0000

    Add project files.

---
+++ array_task2.py
@@ -0,0 +1,8 @@
+import numpy as ma
+a=ma.array([5.2,6.8,7.7,9.1,13,15.1,18.2,18.3,14.5,12.7,9.1,4])
+print(min(a))
+print(max(a))
+d= sum(a)
+print(d)
+e = d/12
+print("the average of the weather in the UK in 2023 is ",e)
